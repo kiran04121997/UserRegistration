80699399054eb705a77d6866865afc98d27f12a5
kiran04121997 <bhosalekiran259@gmail.com>
2022-02-14 18:17:20 +0530

    [kiran] mobile number

--- UserRegistration/UnitTest1.cs
+++ UserRegistration/UnitTest1.cs
@@ -27,32 +27,16 @@ namespace UserRegistration
         public void TestLastName_ReturnTrue()
         {
             string excepted = "true";
-            var actual = UserRegister.ValidateLastName("Kumar");
+            var actual = UserRegister.ValidateFirstName("Kumar");
             Assert.AreEqual(excepted, actual);
         }
         [TestMethod]
         public void TestLastName_ReturnFalse()
         {
             string excepted = "false";
-            var actual = UserRegister.ValidateLastName("Ka");
+            var actual = UserRegister.ValidateFirstName("Ka");
             Assert.AreEqual(excepted, actual);
-            var actual1 = UserRegister.ValidateLastName("kumar");
-            Assert.AreEqual(excepted, actual1);
-        }
-        [TestMethod]
-        public void TestEmailID_ReturnTrue()
-        {
-            string excepted = "true";
-            var actual = UserRegister.ValidateEmailID("saurav@gmail.co.in");
-            Assert.AreEqual(excepted, actual);
-        }
-        [TestMethod]
-        public void TestEmailID_ReturnFalse()
-        {
-            string excepted = "false";
-            var actual = UserRegister.ValidateEmailID("saurav.kr@g.in");
-            Assert.AreEqual(excepted, actual);
-            var actual1 = UserRegister.ValidateEmailID(".kr@gmail.com.in");
+            var actual1 = UserRegister.ValidateFirstName("kumar");
             Assert.AreEqual(excepted, actual1);
         }
         [TestMethod]
@@ -72,20 +56,6 @@ namespace UserRegistration
             Assert.AreEqual(excepted, actual1);
             var actual2 = UserRegister.ValidateMobileNumber("91 70678454858");
             Assert.AreEqual(excepted, actual2);
-        }
-        [TestMethod]
-        public void TestPassword_ReturnTrue()
-        {
-            string excepted = "true";
-            var actual = UserRegister.ValidatePassword("SauGav76@#$");
-            Assert.AreEqual(excepted, actual);
-        }
-        [TestMethod]
-        public void TestPassword_ReturnFalse()
-        {
-            string excepted = "false";
-            var actual = UserRegister.ValidatePassword("S9a@#$j");
-            Assert.AreEqual(excepted, actual);
         }
     }
 
--- UserRegistrationProject/UserRegistrationProject/Program.cs
+++ UserRegistrationProject/UserRegistrationProject/Program.cs
@@ -8,6 +8,10 @@ namespace UserRegistrationProject
 {
     public class Program
     {
+        /* UC-4-UserRegistration
+         * Validate Mobile Number starts with Country Code and have atlest 1 space after code.
+         * and then after country code it should have 10 numbers only.
+         */
         //Method to perform UserRegistration
         public static void UserRegistration()
         {
@@ -15,7 +19,7 @@ namespace UserRegistrationProject
             while (Continue)
             {
                 Console.WriteLine("Choose Option to Validate with their specified Pattern");
-                Console.WriteLine("1 = First Name\n2 = Last Name\n3 =Email ID\n4 = Mobile Number\n5 = Password\n0 = Exit");
+                Console.WriteLine("1 = First Name\n2 = Last Name\n3=MobileNumber\n0=Exit");
                 int choice = int.Parse(Console.ReadLine());
                 switch (choice)
                 {
@@ -33,19 +37,9 @@ namespace UserRegistrationProject
                         Console.WriteLine(UserRegister.ValidateFirstName(lastName));
                         break;
                     case 3:
-                        Console.WriteLine("Enter Email ID");
-                        string email = Console.ReadLine();
-                        Console.WriteLine(UserRegister.ValidateEmailID(email));
-                        break;
-                    case 4:
                         Console.WriteLine("Enter Mobile Number");
                         string mobileNumber = Console.ReadLine();
                         Console.WriteLine(UserRegister.ValidateMobileNumber(mobileNumber));
-                        break;
-                    case 5:
-                        Console.WriteLine("Enter PassWord");
-                        string password = Console.ReadLine();
-                        Console.WriteLine(UserRegister.ValidatePassword(password));
                         break;
                     default:
                         Console.WriteLine("Enter Correct Option!");
