62b8ffd284ab1add823b7b6af9161b5be24aae05
Kiran Bhosale <91077158+kiran04121997@users.noreply.github.com>
2022-03-02 11:06:30 +0530

    update

--- UserRegistration/UnitTest1.cs
+++ UserRegistration/UnitTest1.cs
@@ -11,64 +11,64 @@ namespace UserRegistration
         public void TestFirstName_ReturnTrue()
         {
             string excepted = "true";
-            var actual = UserRegister.ValidateFirstName("Saurav");
+            var actual = UserRegister.ValidateFirstName("Kiran");
             Assert.AreEqual(excepted, actual);
         }
         [TestMethod]
         public void TestFirstName_ReturnFalse()
         {
             string excepted = "false";
-            var actual = UserRegister.ValidateFirstName("Sa");
+            var actual = UserRegister.ValidateFirstName("ki");
             Assert.AreEqual(excepted, actual);
-            var actual1 = UserRegister.ValidateFirstName("saurav");
+            var actual1 = UserRegister.ValidateFirstName("kiran");
             Assert.AreEqual(excepted, actual1);
         }
         [TestMethod]
         public void TestLastName_ReturnTrue()
         {
             string excepted = "true";
-            var actual = UserRegister.ValidateLastName("Kumar");
+            var actual = UserRegister.ValidateLastName("bhosale");
             Assert.AreEqual(excepted, actual);
         }
         [TestMethod]
         public void TestLastName_ReturnFalse()
         {
             string excepted = "false";
-            var actual = UserRegister.ValidateLastName("Ka");
+            var actual = UserRegister.ValidateLastName("bh");
             Assert.AreEqual(excepted, actual);
-            var actual1 = UserRegister.ValidateLastName("kumar");
+            var actual1 = UserRegister.ValidateLastName("bhosale");
             Assert.AreEqual(excepted, actual1);
         }
         [TestMethod]
         public void TestMobileNumber_ReturnTrue()
         {
             string excepted = "true";
-            var actual = UserRegister.ValidateMobileNumber("91 7067845485");
+            var actual = UserRegister.ValidateMobileNumber("91 9921925459");
             Assert.AreEqual(excepted, actual);
         }
         [TestMethod]
         public void TestMobileNumber_ReturnFalse()
         {
             string excepted = "false";
-            var actual = UserRegister.ValidateMobileNumber("7067845485");
+            var actual = UserRegister.ValidateMobileNumber("9921925459");
             Assert.AreEqual(excepted, actual);
-            var actual1 = UserRegister.ValidateMobileNumber("23 7067845485");
+            var actual1 = UserRegister.ValidateMobileNumber("23 9921925459");
             Assert.AreEqual(excepted, actual1);
-            var actual2 = UserRegister.ValidateMobileNumber("91 70678454858");
+            var actual2 = UserRegister.ValidateMobileNumber("91 9921925459");
             Assert.AreEqual(excepted, actual2);
         }
         [TestMethod]
         public void TestPassword_ReturnTrue()
         {
             string excepted = "true";
-            var actual = UserRegister.ValidatePassword("SauGav76");
+            var actual = UserRegister.ValidatePassword("kiran@992192");
             Assert.AreEqual(excepted, actual);
         }
         [TestMethod]
         public void TestPassword_ReturnFalse()
         {
             string excepted = "false";
-            var actual = UserRegister.ValidatePassword("S9aura");
+            var actual = UserRegister.ValidatePassword("k9aura");
             Assert.AreEqual(excepted, actual);
         }
     }
